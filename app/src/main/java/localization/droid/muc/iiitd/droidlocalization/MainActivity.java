426c12afcd76a13998400b7fcadf2b580c7b7381
Sonia <sonias@iiitd.ac.in>
2015-01-17 19:07:00 +0530

    Textview added

--- app/src/main/java/localization/droid/muc/iiitd/droidlocalization/MainActivity.java
+++ app/src/main/java/localization/droid/muc/iiitd/droidlocalization/MainActivity.java
@@ -21,7 +21,7 @@ import com.google.android.gms.location.LocationListener;
 public class MainActivity extends Activity implements
         LocationListener,GooglePlayServicesClient.ConnectionCallbacks,
         GooglePlayServicesClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private final String TAG = "MyAwesomeApp";
+    private final String TAG = "LocalizationDroid";
 
     private TextView mLocationView;
 
@@ -33,7 +33,7 @@ public class MainActivity extends Activity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationView = new TextView(this);
+        mLocationView = (TextView) findViewById(R.id.textlocation);
 
         //setContentView(mLocationView);
         int resp = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
@@ -47,7 +47,7 @@ public class MainActivity extends Activity implements
         }
         else{
                 Log.d(TAG,"Google play service not installed");
-            this.onDestroy();
+                this.onDestroy();
         }
 
     }
@@ -72,18 +72,8 @@ public class MainActivity extends Activity implements
         mLocationRequest = LocationRequest.create();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         mLocationRequest.setInterval(10000); // Update location every 10 second
-
-        Location m = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
 
-        if(m!=null)
-        {
-            Log.d(TAG, Double.toString(m.getLatitude()));
-            Log.d(TAG, Double.toString(m.getLongitude()));
-
-        }
-        else
-            Log.d(TAG,"null");
 
     }
 
@@ -106,6 +96,10 @@ public class MainActivity extends Activity implements
     public void onLocationChanged(Location location) {
         Log.i(TAG, Double.toString(location.getLatitude()));
         Log.i(TAG, Double.toString(location.getLongitude()));
-        mLocationView.setText("Location received: " + location.toString());
+        //mLocationView.setText("Location received: " + location.toString());
+
+        String v =  "Latitude :" + Double.toString(location.getLatitude()) + "\nLongitude : " + Double.toString(location.getLongitude()) + "\n";
+        v += mLocationView.getText();
+        mLocationView.setText(v);
     }
 }
